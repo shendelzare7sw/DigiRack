f67c5feeb6f72dca5f1c6147455276a3c9724aef
Tabah Ujianto <shendelzare7sw@gmail.com>
2026-05-14 11:47:20 +0700

    Update logo asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'DigiRack') }} - {{ $title ?? 'Pusat Peralatan Jaringan & IT' }}</title>
-        <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
-        <link rel="shortcut icon" type="image/png" href="{{ asset('images/logo.png') }}">
-        <link rel="apple-touch-icon" href="{{ asset('images/logo.png') }}">
+        <link rel="icon" type="image/jpeg" href="{{ asset('images/logo.jpg') }}">
+        <link rel="shortcut icon" type="image/jpeg" href="{{ asset('images/logo.jpg') }}">
+        <link rel="apple-touch-icon" href="{{ asset('images/logo.jpg') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
